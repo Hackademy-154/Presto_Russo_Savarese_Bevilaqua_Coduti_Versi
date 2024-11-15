f066cf1d7b41da9f7605b1d31ff297a348a0d8fd
Alessandro G. Russo <alessandrogustavorusso@gmail.com>
2024-11-15 15:18:08 +0100

    Rimosso tasti di decisione accesso

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,29 +17,9 @@
 @endauth
 @guest
 <div class="text-center py-5">
-    <p id="questionTitle">Sei già registrato?</p>
-    <div class="button-group">
-        <button class="btn btn-custom" id="yesButton">Sì</button>
-        <button class="btn btn-custom" id="noButton">No</button>
-    </div>
-    
-    <div id="loginButton" class="mt-3" style="display: none;">
-        <a class="btn btn-custom" href="{{ route('login') }}">
-            Accedi
-        </a>
-    </div>
-    
-    <div id="registerButton" class="mt-3" style="display: none;">
-        <a class="btn btn-custom" href="{{ route('register') }}">
-            Registrati
-        </a>
-    </div>
-    
-    <div id="backButton" class="mt-3" style="display: none;">
-        <p id="backTitle" style="display: none;">Se hai sbagliato scelta premi il tasto indietro</p>
-        <button class="btn btn-custom" id="backToChoices">Indietro</button>
-    </div>
-</div>
+    <a class="btn btn-custom" href="{{ route('register') }}">
+        Registrati
+    </a>
 @endguest
 
 </div>
