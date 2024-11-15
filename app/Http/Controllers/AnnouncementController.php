75ff962696c879f9d4074e4e7c2cd81cee5a129f
Alessandro G. Russo <alessandrogustavorusso@gmail.com>
2024-11-15 12:33:23 +0100

    creato MIDDLEWARE per PAGINA CREA ARTICOLO

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class AnnouncementController extends Controller {
+class AnnouncementController extends Controller implements HasMiddleware {
+
+    public static function middleware(): array {
+        return [
+            'auth',
+            new Middleware( 'log', only: [ 'announcements.create' ] ),
+            // new Middleware( 'subscribed', except: [ 'store' ] ),
+        ];
+    }
+
     public function create() {
         return view( 'announcements.create' );
     }
