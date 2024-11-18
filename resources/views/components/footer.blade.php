4705d4975763c99f62bc0b610d207ed8c657cdaf
DragonautMk <michelesavarese1997@gmail.com>
2024-11-18 14:30:42 +0100

    lavoro sul footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,27 +1,95 @@
-<div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-            </a>
-            <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+<footer class="py-5 container-fluid background-footer">
+    <div class="px-3 row justify-content-between">
+        <!-- Colonna Aiuto & Contatti -->
+        <div class="col-6 col-md-2 mb-3">
+            <h5 class="text-nowrap"><i class="bi bi-question-circle"></i> Aiuto & Contatti</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pagamenti</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contanti</a></li>
+            </ul>
         </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24"
+        <!-- Colonna Partners -->
+        <div class="col-6 col-md-2 mb-3">
+            <h5><i class="bi bi-truck"></i> Partners</h5>
+            <ul class="nav flex-nowrap ">
+                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary"><img class="img-fluid"
+                            src="{{asset('icons/DHL_Group_06.2023.svg.png ')}}" alt="dhl"></a></li>
+
+                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary">
+                    <img
+                    class="img-fluid" src="{{asset('
+icons/Web-Ready-Small-DPD-icon.jpg
+')}}
+                    " alt="dpd">
+                </a>
+
+
+                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary">
+                    <img
+                    class="img-fluid" src="{{asset('
+icons/fedex-logo-free-download-free-vector-1.jpg
+
+')}}
+                    " alt="dpd">
+
+                </a>
+
+                </li>
+                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary">
+                    <img
+                    class="img-fluid" src="{{asset('
+icons/Logo-GLS-1080x1080px.svg.png
+
+')}}
+                    " alt="dpd">
+
+                </a>
+
+                </li>
+
+            </ul>
+        </div>
+
+        <!-- Colonna Policy -->
+        <div class="col-6 col-md-2 mb-3">
+            <h5><i class="bi bi-info-circle"></i> Policy</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Termini di
+                        servizio</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Cookies</a></li>
+            </ul>
+        </div>
+
+        <!-- Colonna Social -->
+        <div class="col-12 col-md-4 mb-3">
+            <h5 class="text-center">SEGUICI SUI SOCIAL</h5>
+            <ul class="list-unstyled d-flex justify-content-center align-items-center">
+                <li class="ms-3 px-3"><a class="link-body-emphasis" href="#"><i class="bi bi-facebook"
+                            style="font-size: 1.5rem;"></i></a></li>
+                <li class="ms-3 px-3"><a class="link-body-emphasis" href="#"><i class="bi bi-twitter-x"
+                            style="font-size: 1.5rem;"></i></a></li>
+                <li class="ms-3 px-3"><a class="link-body-emphasis" href="#"><i class="bi bi-instagram"
+                            style="font-size: 1.5rem;"></i></a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+        <p>© 2024 Company, Inc. All rights reserved.</p>
+        <ul class="list-unstyled d-flex">
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24"
                         height="24">
                         <use xlink:href="#twitter"></use>
                     </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24"
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24"
                         height="24">
                         <use xlink:href="#instagram"></use>
                     </svg></a></li>
-            <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24"
+            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24"
                         height="24">
                         <use xlink:href="#facebook"></use>
                     </svg></a></li>
         </ul>
-    </footer>
-</div>
+    </div>
+</footer>
