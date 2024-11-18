9e618548474f613bb8ef15ff4918adb13d3755df
DragonautMk <michelesavarese1997@gmail.com>
2024-11-18 16:27:07 +0100

    completato footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,95 +1,75 @@
-<footer class="py-5 container-fluid background-footer">
+<footer class="py-3 container-fluid background-footer">
     <div class="px-3 row justify-content-between">
-        <!-- Colonna Aiuto & Contatti -->
-        <div class="col-6 col-md-2 mb-3">
-            <h5 class="text-nowrap"><i class="bi bi-question-circle"></i> Aiuto & Contatti</h5>
+        <!-- Aiuto & Contatti -->
+        <div class="col-12 col-md-2 mb-2">
+            <h5 class="text-nowrap text-center fw-bold"><i class="bi bi-question-circle"></i> Aiuto & Contatti</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pagamenti</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contanti</a></li>
+                <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-body-secondary">Pagamenti</a></li>
+                <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-body-secondary">Contanti</a></li>
+                <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-body-secondary">Lavora con noi</a></li>
             </ul>
         </div>
 
-        <!-- Colonna Partners -->
-        <div class="col-6 col-md-2 mb-3">
-            <h5><i class="bi bi-truck"></i> Partners</h5>
-            <ul class="nav flex-nowrap ">
-                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary"><img class="img-fluid"
-                            src="{{asset('icons/DHL_Group_06.2023.svg.png ')}}" alt="dhl"></a></li>
-
-                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary">
-                    <img
-                    class="img-fluid" src="{{asset('
-icons/Web-Ready-Small-DPD-icon.jpg
-')}}
-                    " alt="dpd">
-                </a>
-
-
-                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary">
-                    <img
-                    class="img-fluid" src="{{asset('
-icons/fedex-logo-free-download-free-vector-1.jpg
-
-')}}
-                    " alt="dpd">
-
-                </a>
-
+        <!-- Partners -->
+        <div class="col-12 col-md-2 mb-2">
+            <h5 class="fw-bold"><i class="bi bi-truck"></i> Partners</h5>
+            <ul class="nav flex-wrap justify-content-center">
+                <li class="nav-item mx-1 my-1"><a href="#" class="nav-link p-0"><img class="img-fluid footer-icon-size dhl-iconsize"
+                            src="{{ asset('icons/DHL_Group_06.2023.svg.png') }}" alt="dhl"></a></li>
+                <li class="nav-item mx-1 my-1"><a href="#" class="nav-link p-0"><img class="img-fluid footer-icon-size"
+                            src="{{ asset('icons/Web-Ready-Small-DPD-icon.jpg') }}" alt="dpd"></a></li>
+                <li class="nav-item mx-1 my-1"><a href="#" class="nav-link p-0"><img class="img-fluid footer-icon-size"
+                            src="{{ asset('icons/fedex-logo-free-download-free-vector-1.jpg') }}" alt="fedex"></a>
                 </li>
-                <li class="nav-item mb-2 px-1"><a href="#" class="nav-link p-0 text-body-secondary">
-                    <img
-                    class="img-fluid" src="{{asset('
-icons/Logo-GLS-1080x1080px.svg.png
-
-')}}
-                    " alt="dpd">
-
-                </a>
-
-                </li>
-
+                <li class="nav-item mx-1 my-1"><a href="#" class="nav-link p-0"><img class="img-fluid footer-icon-size"
+                            src="{{ asset('icons/Logo-GLS-1080x1080px.svg.png') }}" alt="gls"></a></li>
             </ul>
         </div>
 
-        <!-- Colonna Policy -->
-        <div class="col-6 col-md-2 mb-3">
-            <h5><i class="bi bi-info-circle"></i> Policy</h5>
+        <!-- Policy -->
+        <div class="col-12 col-md-2 mb-2">
+            <h5 class="fw-bold"><i class="bi bi-info-circle"></i> Policy</h5>
             <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Termini di
+                <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-body-secondary">Termini di
                         servizio</a></li>
-                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Cookies</a></li>
+                <li class="nav-item mb-1"><a href="#" class="nav-link p-0 text-body-secondary">Cookies</a></li>
             </ul>
         </div>
 
-        <!-- Colonna Social -->
-        <div class="col-12 col-md-4 mb-3">
-            <h5 class="text-center">SEGUICI SUI SOCIAL</h5>
+        <!-- Social -->
+        <div class="col-12 col-md-4 mb-2">
+            <h5 class="text-center fw-bold">SEGUICI SUI SOCIAL</h5>
             <ul class="list-unstyled d-flex justify-content-center align-items-center">
-                <li class="ms-3 px-3"><a class="link-body-emphasis" href="#"><i class="bi bi-facebook"
+                <li class="mx-2"><a href="#"><i class="text-black bi bi-facebook"
                             style="font-size: 1.5rem;"></i></a></li>
-                <li class="ms-3 px-3"><a class="link-body-emphasis" href="#"><i class="bi bi-twitter-x"
+                <li class="mx-2"><a href="#"><i class="text-black bi bi-twitter-x"
                             style="font-size: 1.5rem;"></i></a></li>
-                <li class="ms-3 px-3"><a class="link-body-emphasis" href="#"><i class="bi bi-instagram"
+                <li class="mx-2"><a href="#"><i class="text-black bi bi-instagram"
                             style="font-size: 1.5rem;"></i></a></li>
             </ul>
         </div>
     </div>
 
-    <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p>© 2024 Company, Inc. All rights reserved.</p>
-        <ul class="list-unstyled d-flex">
-            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#twitter"></use>
-                    </svg></a></li>
-            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#instagram"></use>
-                    </svg></a></li>
-            <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24"
-                        height="24">
-                        <use xlink:href="#facebook"></use>
-                    </svg></a></li>
-        </ul>
+    <!-- Flags e Copyright -->
+    <div class="d-flex flex-wrap justify-content-between align-items-center py-2 border-top">
+        <div class="d-flex flex-wrap justify-content-center">
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-islanda-circolare-96.png') }}" alt="Islanda"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-usa-circolare-96.png') }}" alt="America"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-circolare-tedesca-96.png') }}" alt="Germania"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-spagna2-circolare-96.png') }}" alt="Spagna"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-circolare-olandese-96.png') }}" alt="Olanda"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-belgio-circolare-96.png') }}" alt="Belgio"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-france-96.png') }}" alt="Francia"></a>
+            <a class="mx-2" href="#"><img class="img-fluid flag-size"
+                    src="{{ asset('icons/icons8-circolare-italia-48.png') }}" alt="Italia"></a>
+        </div>
+        <p class="mx-3 text-center fw-bold justify-item-center">© 2024 Presto.it</p>
     </div>
 </footer>
