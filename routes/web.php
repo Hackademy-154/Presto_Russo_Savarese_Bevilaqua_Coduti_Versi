2fa672bc5c8384b6eb0266f4fee161f6bafb5fef
Gennaro Versi <gennaroversiprog@outlook.it>
2024-11-19 11:52:12 +0100

    middlewareCreazione

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get( '/', [ PublicController::class, 'home' ] )->name( 'home.page' );
-Route::get( '/announcements/create', [ AnnouncementController::class, 'create' ] )->name( 'announcements.create' );
+Route::get( '/announcements/create', [ AnnouncementController::class, 'create' ] )->middleware( 'auth' )->name( 'announcements.create' );
 Route::get( '/announcements/index', [ AnnouncementController::class, 'index' ] )->name( 'announcements.index' );
 Route::get( '/announcements/{announcement}', [ AnnouncementController::class, 'show' ] )->name( 'announcements.show' );
 
