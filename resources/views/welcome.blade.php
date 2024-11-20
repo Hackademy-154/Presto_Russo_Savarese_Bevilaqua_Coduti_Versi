7c01503e3ab33ce0ead451ba15c5d6f73e6db62b
DragonautMk <michelesavarese1997@gmail.com>
2024-11-20 12:15:18 +0100

    sistemata parte della home e aggiustamenti generali

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,35 +11,31 @@
         </div>
     @endif
 
-    {{-- //! Non cancellare, WIP --}}
-    <div class="hero-section d-flex align-items-center">
-        <div class="container-fluid">
-            <div class="row w-100">
-                <!-- Colonna Sinistra: Testo -->
-                <div class="col-12 col-md-6 text-start text-light position-relative">
-                    <div class="hero-text-box p-4">
-                        <h1 class="fw-bold">Trova ciò che <span class="highlight">desideri</span>, vendi ciò<br> che non
-                            usi.</h1>
-                        <p>Con <span class="highlight">Presto.it</span> è semplice e veloce!</p>
+    {{-- <div class=" d-flex align-items-center border border-primary"> --}}
+    <div class="container-fluid hero-section">
+        <div class="row justify-content-center p-0 m-0">
+            <div class="col-12 col-md-3 text-start text-light h-100 landingbox1 m-0 p-0">
+                <div class="col-12 hero-text-box p-4">
+                    <h1>Trova ciò che <span class="fw-bolder">desideri</span>, vendi ciò<br> che non
+                        usi.</h1>
+                    <h1>Con <span class="fw-bolder">presto.it</span> è semplice e veloce!</p>
                         <a href="#" class="btn btn-hero mt-3">Inizia a vendere</a>
-                    </div>
                 </div>
-                <!-- Colonna Destra: Immagine -->
-                <div class="col-12 col-md-6">
-                    <img src="{{ asset('images/sfondo.jpeg') }}" alt="Hero Image" class="img-fluid">
-                </div>
+            </div>
+            <div class="d-flex col-12 col-md-6 w-50 m-0 p-0 center-sd">
+                <img src="{{ asset('images/landing.jpeg') }}" alt="Landing" class="h-100">
             </div>
         </div>
     </div>
+    {{-- </div> --}}
 
 
     <!-- Prima Sezione -->
     <div class="container my-5">
         <div class="row align-items-center mb-5">
-            <!-- Prima Categoria -->
             <div class="col-12 col-md-6 text-center">
                 <div class="category-box">
-                    <img src="{{ asset('images/sedia.jpg') }}" alt="Immagine Categoria 1" class="category-img">
+                    <img src="{{ asset('images/char.jpg') }}" alt="Immagine Categoria 1" class="category-img ">
                 </div>
             </div>
             <div class="col-12 col-md-6">
@@ -52,7 +48,6 @@
         </div>
 
         <div class="row align-items-center">
-            <!-- Seconda Categoria -->
             <div class="col-12 col-md-6 order-md-2 text-center">
                 <div class="category-box">
                     <img src="{{ asset('images/shopping.jpg') }}" alt="Immagine Categoria 2" class="category-img">
@@ -72,18 +67,14 @@
     <section class="latest-announcements">
         <div class="container text-center">
             <h2 class="section-title">Ecco i nostri ultimi sei annunci</h2>
-            <div class="row justify-content-center">
+            <div class="row justify-content-center px-5">
                 @forelse($announcements as $announcement)
                     <div class="col-12 col-md-4 col-lg-2 announcement-card">
                         <div class="card">
-                            <!-- Immagine dell'annuncio -->
                             <img src="{{ $announcement->image }}" class="card-img-top" alt="{{ $announcement->title }}">
                             <div class="card-body">
-                                <!-- Titolo dell'annuncio -->
                                 <h5 class="card-title">{{ $announcement->title }}</h5>
-                                <!-- Descrizione dell'annuncio -->
                                 <p class="card-text">{{ $announcement->description }}</p>
-                                <!-- Prezzo dell'annuncio -->
                                 <p class="card-price">{{ $announcement->price }} €</p>
                             </div>
                         </div>
