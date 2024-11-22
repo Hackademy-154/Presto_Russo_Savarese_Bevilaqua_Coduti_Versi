67ec391e3e409546d4711478702293749f380bd3
Alessandro G. Russo <alessandrogustavorusso@gmail.com>
2024-11-22 17:20:58 +0100

    Aggiunta traduzione in tutta la welcome

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -20,8 +20,28 @@ return
 'login'=>'Login',
 'logout'=>'Logout',
 'SelectCategory'=>'Select a Category',
-    
-    
+'startselling'=>'Start Selling',
+'buyNow'=>'Buy Now',
+'sixAnnouncements'=>'Here are our last six announcements',
+'noAnnouncements'=>'There are no announcements available at the moment.',
+'viewAnnouncements'=>'View Announcements',
+
+
+    // frasi con span
+'frase0pz1'=>'Find what',
+'frase0pz2'=>'desires',
+'frase0pz3'=>'Sell what',
+'frase0pz4'=>'you don\'t use',
+'frase0pz5'=>'With',
+'frase0pz6'=>'it is simple and fast',
+
+'frase1pz1'=>'Can\'t find the right ',
+'frase1pz2'=>'combination',
+'frase1pz3'=>'for your home',
+'frase1pz4'=>'Add character and harmony to your spaces with a few simple details',
+'frase2pz1'=>'Valorize every moment with',
+'frase2pz2'=>'style',
+'frase3'=>'A touch of elegance and personality to your look, with unique details that enhance every moment',
     
     
     
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,10 +23,9 @@
         <div class="row justify-content-center p-0 m-0">
             <div class="col-12 col-md-3 text-start text-light h-100 landingbox1 m-0 p-0">
                 <div class="col-12 hero-text-box p-4">
-                    <h1>Trova ciò che <span class="fw-bolder">desideri</span>, vendi ciò<br> che non
-                        usi.</h1>
-                    <h1>Con <span class="fw-bolder">presto.it</span> è semplice e veloce!</p>
-                        <a href="#" class="btn btn-hero mt-3">Inizia a vendere</a>
+                    <h1>{{__('ui.frase0pz1')}} <span class="fw-bolder">{{__('ui.frase0pz2')}}</span>,{{__('ui.frase0pz3')}}<br> {{__('ui.frase0pz4')}}.</h1>
+                    <h1>{{__('ui.frase0pz5')}} <span class="fw-bolder">presto.it</span> {{__('ui.frase0pz6')}} !</h1>
+                        <a href="#" class="btn btn-hero mt-3">{{__('ui.startselling')}}</a>
                 </div>
             </div>
             <div class="d-flex col-12 col-md-6 w-50 m-0 p-0 center-sd">
@@ -46,11 +45,11 @@
                 </div>
             </div>
             <div class="col-12 col-md-6">
-                <h3 class="category-title">Non riesci a trovare le giuste <span class="highlight">combinazioni</span>
-                    per la tua casa?</h3>
-                <p class="category-description">Aggiungi carattere e armonia ai tuoi spazi con pochi semplici dettagli!
+                <h3 class="category-title">{{__('ui.frase1pz1')}} <span class="highlight">{{__('ui.frase1pz2')}}</span>
+                    {{__('ui.frase1pz3')}}?</h3>
+                <p class="category-description">{{__('ui.frase1pz4')}}!
                 </p>
-                <a href="#" class="btn btn-custom">Acquista ora</a>
+                <a href="#" class="btn btn-custom">{{__('ui.buyNow')}}</a>
             </div>
         </div>
 
@@ -61,10 +60,9 @@
                 </div>
             </div>
             <div class="col-12 col-md-6 order-md-1">
-                <h3 class="category-title">Valorizza ogni momento con <span class="highlight">stile!</span></h3>
-                <p class="category-description">Tocco di eleganza e personalità al tuo look, con dettagli unici che
-                    valorizzano ogni momento.</p>
-                <a href="#" class="btn btn-custom">Acquista ora</a>
+                <h3 class="category-title">{{__('ui.frase2pz1')}}<span class="highlight"> {{__('ui.frase2pz2')}}!</span></h3>
+                <p class="category-description"> {{__('ui.frase3')}}.</p>
+                <a href="#" class="btn btn-custom">{{__('ui.buyNow')}}</a>
             </div>
         </div>
     </div>
@@ -73,7 +71,7 @@
     {{-- Sezione ultimi annunci --}}
     <section class="latest-announcements">
         <div class="container text-center">
-            <h2 class="section-title">Ecco i nostri ultimi sei annunci</h2>
+            <h2 class="section-title">{{__('ui.sixAnnouncements')}}</h2>
             <div class="row justify-content-center px-5">
                 @forelse($announcements as $announcement)
                 <div class="col-12 col-md-6 col-lg-4 announcement-card">
@@ -91,11 +89,11 @@
                 </div>
                 @empty
                     <div class="col-12">
-                        <p class="no-announcements">Non ci sono annunci disponibili al momento.</p>
+                        <p class="no-announcements">{{__('ui.noAnnouncements')}}</p>
                     </div>
                 @endforelse
             </div>
-            <a href="{{ route('announcements.index') }}" class="btn btn-primary mt-4">Vedi Annunci</a>
+            <a href="{{ route('announcements.index') }}" class="btn btn-primary mt-4">{{__('ui.viewAnnouncements')}}</a>
         </div>
     </section>
 </x-layout>
