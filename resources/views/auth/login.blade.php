b8636803976b97951335cd1d7b0f00de5fb2b2e9
DragonautMk <michelesavarese1997@gmail.com>
2024-11-22 12:31:52 +0100

    frontendato login, register, aggiustamente vari e creato errore 404

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+{{-- <x-layout>
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="col-12 col-md-6 text-center mt-5">
@@ -42,4 +42,41 @@
 </div>
 
     </div>
+</x-layout> --}}
+
+
+<x-layout>
+    {{-- <body class="body-login"> --}}
+        <div class="container login-container mt-3">
+            <div class=" row login-box shadow justify-content-center">
+                <!-- Colonna Immagine -->
+                <div class="col-12 col-md-6 login-image-column d-flex justify-content-center align-items-center">
+                    <img src="{{ asset('images/accedi.jpeg') }}" alt="Login Illustration" class="img-fluid">
+                </div>
+                <!-- Colonna Form -->
+                <div class="col-12 col-md-6 login-form-column align-items-center">
+                    <h1 class="login-title">Bentornato!</h1>
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
+                        <div class="form-group pt-2 pb-2">
+                            <label for="email">Indirizzo email</label>
+                            <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" placeholder="Inserisci la tua email">
+                            @error('email')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="form-group pt-2 pb-2">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" placeholder="Inserisci la tua password">
+                            @error('password')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-custom pt-2 pb-2">Accedi a presto.it</button>
+                        <p class="text-center mt-3 pb-3 pt-3">Non hai già un account? <a href="{{ route('register') }}" class="link">Registrati</a></p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    {{-- </body> --}}
 </x-layout>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>404</h1>
+            <p>Ops! La pagina che stai cercando non esiste.</p>
+            <a href="{{ url('/') }}">Torna alla home</a>
+        </div>
+    </div>
+</div>
+</x-layout>
