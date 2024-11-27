3d27130c9c17e731ac80123f9a252247a47cf199
Gennaro Versi <gennaroversiprog@outlook.it>
2024-11-27 16:09:24 +0100

    CorrettoTutto

--- app/Jobs/GoogleVisionLabelImage.php
+++ app/Jobs/GoogleVisionLabelImage.php
@@ -29,8 +29,8 @@ class GoogleVisionLabelImage implements ShouldQueue {
         if ( ! $i ) {
             return;
         }
-        $image = file_get_contents( storage_path( '/app/' . $i->path ) );
-        putenv( 'GOOGLE_APPLICATION_CREDENTIALS=' . base_path( 'google_credentials.json' ) );
+        $image = file_get_contents( storage_path( 'app/public/' . $i->path ) );
+        putenv( 'GOOGLE_APPLICATION_CREDENTIALS=' . base_path( 'google_credential.json' ) );
         $imageAnnotator = new ImageAnnotatorClient();
         $response = $imageAnnotator->labelDetection( $image );
         $labels = $response->getLabelAnnotations();
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -19,65 +19,59 @@
                 </div>
             </div>
         </div>
-    </div>
+
         {{-- INIZIO GOOGLE AI --}}
-<div>
-    @foreach ($announcement_to_check->images as $key => $image)
-    <div class="col-md-5 ps-3">
-        <div class="card-body">
-            <h5>Labels</h5>
-            @if($image->labels){
-                @foreach($image->labels as $label)
-                #{{$label}},
+        @if ($announcement_to_check)
+            <div class="row">
+                @foreach ($announcement_to_check->images as $key => $image)
+                    <div class="col-md-6">
+                        {{-- Immagine --}}
+                        <div class="card mb-4 shadow">
+                            <img src="{{ $image->getUrl(500, 700) }}" 
+                                 class="card-img-top img-fluid" 
+                                 alt="Immagine {{ $key + 1 }}">
+                            <div class="card-body">
+                                {{-- Labels --}}
+                                <h5>Labels</h5>
+                                @if($image->labels && count($image->labels) > 0)
+                                    <p>
+                                        @foreach($image->labels as $label)
+                                            <span class="badge bg-primary">#{{ $label }}</span>
+                                        @endforeach
+                                    </p>
+                                @else
+                                    <p class="fst-italic">No labels available</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- Rating --}}
+                    <div class="col-md-6">
+                        <div class="card mb-4 shadow">
+                            <div class="card-body">
+                                <h5 class="card-title">Ratings</h5>
+                                @php
+                                    $categories = ['adult', 'violence', 'racy', 'medical', 'spoof'];
+                                @endphp
+                                @foreach ($categories as $category)
+                                    <div class="row align-items-center mb-2">
+                                        <div class="col-2">
+                                            <div class="rounded-circle {{ $image->$category }}" style="width: 20px; height: 20px; background-color: {{ $image->$category ? 'red' : 'green' }};"></div>
+                                        </div>
+                                        <div class="col-10 text-capitalize">
+                                            {{ $category }}
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
-            }@else
-            <p class="fst-italic">No labels</p>
-            @endif
-        </div>
-    </div>
-    <div class="col-md-3 ">
-        <div class="card-body">
-            <h5 class="card-title">Rating</h5>
-            <div class="row justify-content-center">
-                <div class="col-2 ">
-                    <div class="text-center mx-auto {{$image->adult}}">
-                    </div>
-                </div>
-                <div class="col-10">adult</div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-2">
-                    <div class="text-center mx-auto {{$image->violence}}">
-                    </div>
-                </div>
-                <div class="col-10">violence</div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-2">
-                    <div class="text-center mx-auto {{$image->racy}}">
-                    </div>
-                </div>
-                <div class="col-10">racy</div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-2">
-                    <div class="text-center mx-auto {{$image->medical}}">
-                    </div>
-                </div>
-                <div class="col-10">medical</div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-2">
-                    <div class="text-center mx-auto {{$image->spoof}}">
-                    </div>
-                </div>
-                <div class="col-10">spoof</div>
-            </div>
-        </div> 
-    </div>
-    @endforeach
-</div>
-{{-- FINE GOOGLE Ai --}}
+        @endif
+        {{-- FINE GOOGLE AI --}}
+
         {{-- Se ci sono annunci da revisionare --}}
         @if ($announcement_to_check)
             <div class="row mt-5 align-items-start">
@@ -87,14 +81,14 @@
                         <div class="carousel-inner">
                             @foreach ($announcement_to_check->images as $key => $image)
                                 <div class="carousel-item {{ $key == 0 ? 'active' : '' }}">
-                                    <img src="{{ Storage::url($image->path) }}"
-                                        class="d-block w-100 img-custom"
-                                        alt="Immagine {{ $key + 1 }} dell'annuncio '{{ $announcement_to_check->title }}'">
+                                    <img src="{{ $image->getUrl(500, 700) }}"
+                                         class="d-block w-100 img-custom"
+                                         alt="Immagine {{ $key + 1 }} dell'annuncio '{{ $announcement_to_check->title }}'">
                                 </div>
                             @endforeach
                         </div>
 
-                        <!-- Controlli -->
+                        {{-- Controlli --}}
                         <button class="carousel-control-prev" type="button" data-bs-target="#fixedCarousel" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
@@ -111,18 +105,18 @@
                     <div class="p-4 bg-light shadow rounded">
                         <h2 class="fw-bold">{{ $announcement_to_check->title }}</h2>
                         <p class="text-muted">{{ $announcement_to_check->description }}</p>
-                        <p class="h5 text-success">{{__('ui.price')}}: €{{ $announcement_to_check->price }}</p>
+                        <p class="h5 text-success">{{ __('ui.price') }}: €{{ $announcement_to_check->price }}</p>
                         <div class="d-flex justify-content-between mt-4">
                             {{-- Pulsanti --}}
                             <form action="{{ route('revisor.reject', ['announcement' => $announcement_to_check]) }}" method="POST">
                                 @csrf
                                 @method('PATCH')
-                                <button type="submit" class="btn btn-outline-danger w-100 me-2 rounded-pill">{{__('ui.reject')}}</button>
+                                <button type="submit" class="btn btn-outline-danger w-100 me-2 rounded-pill">{{ __('ui.reject') }}</button>
                             </form>
                             <form action="{{ route('revisor.accept', ['announcement' => $announcement_to_check]) }}" method="POST">
                                 @csrf
                                 @method('PATCH')
-                                <button type="submit" class="btn btn-outline-success w-100 ms-2 rounded-pill">{{__('ui.accept')}}</button>
+                                <button type="submit" class="btn btn-outline-success w-100 ms-2 rounded-pill">{{ __('ui.accept') }}</button>
                             </form>
                         </div>
                     </div>
@@ -132,8 +126,8 @@
             {{-- Nessun annuncio --}}
             <div class="row mt-5">
                 <div class="col-12 text-center">
-                    <h2 class="text-muted fw-bold">{{__('ui.emptyRevisionAnnouncements')}}.</h2>
-                    <a href="{{ route('home.page') }}" class="btn btn-primary rounded-pill mt-4 shadow">{{__('ui.backHome')}}</a>
+                    <h2 class="text-muted fw-bold">{{ __('ui.emptyRevisionAnnouncements') }}.</h2>
+                    <a href="{{ route('home.page') }}" class="btn btn-primary rounded-pill mt-4 shadow">{{ __('ui.backHome') }}</a>
                 </div>
             </div>
         @endif
@@ -143,13 +137,13 @@
             <div class="col-6 col-md-3 text-center">
                 <form action="{{ route('revisor.reviewAccepted') }}" method="GET">
                     @csrf
-                    <button type="submit" class="btn btn-outline-success w-100 py-2 rounded-pill shadow">{{__('ui.announcementsAccepted')}}</button>
+                    <button type="submit" class="btn btn-outline-success w-100 py-2 rounded-pill shadow">{{ __('ui.announcementsAccepted') }}</button>
                 </form>
             </div>
             <div class="col-6 col-md-3 text-center">
                 <form action="{{ route('revisor.reviewRejected') }}" method="GET">
                     @csrf
-                    <button type="submit" class="btn btn-outline-danger w-100 py-2 rounded-pill shadow">{{__('ui.announcementsRejected')}}</button>
+                    <button type="submit" class="btn btn-outline-danger w-100 py-2 rounded-pill shadow">{{ __('ui.announcementsRejected') }}</button>
                 </form>
             </div>
         </div>
